eb69c7d5c494d120312d18d6c59e420dbcd3e71b
Naveen-Singh8783 <naveensingh8783@gmail.com>
2022-05-31 14:23:42 +0530

    OOPS-1

---
+++ Data Structures in Java/OOPS-1/ComplexNumber.java
@@ -0,0 +1,85 @@
+/******************
+ * Question:
+ * A ComplexNumber class contains two data members : one is the real part (R) and the other is imaginary (I) (both integers).
+Implement the Complex numbers class that contains following functions -
+1. constructor
+You need to create the appropriate constructor.
+2. plus -
+This function adds two given complex numbers and updates the first complex number.
+e.g.
+if C1 = 4 + i5 and C2 = 3 +i1
+C1.plus(C2) results in: 
+C1 = 7 + i6 and C2 = 3 + i1
+3. multiply -
+This function multiplies two given complex numbers and updates the first complex number.
+e.g.
+if C1 = 4 + i5 and C2 = 1 + i2
+C1.multiply(C2) results in: 
+C1 = -6 + i13 and C2 = 1 + i2
+4. print -
+This function prints the given complex number in the following format :
+a + ib
+Note : There is space before and after '+' (plus sign) and no space between 'i' (iota symbol) and b.
+ * 
+ * 
+ * Following is the main function we are using internally.
+ * Refer this for completing the ComplexNumbers class
+ * 
+ 
+ public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int real1 = s.nextInt();
+		int imaginary1 = s.nextInt();
+
+		int real2 = s.nextInt();
+		int imaginary2 = s.nextInt();
+
+		ComplexNumbers c1 = new ComplexNumbers(real1, imaginary1);
+		ComplexNumbers c2 = new ComplexNumbers(real2, imaginary2);
+
+		int choice = s.nextInt();
+		 
+		if(choice == 1) {
+			// Add
+			c1.plus(c2);
+			c1.print();
+		}
+		else if(choice == 2) {
+			// Multiply
+			c1.multiply(c2);
+			c1.print();
+		}
+		else {
+			return;
+		}
+	}
+ ******************/
+
+public class ComplexNumbers {
+	// Complete this class
+    int real;
+    int imaginary;
+    
+    public ComplexNumbers(int real, int imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+    
+    public void plus(ComplexNumbers c){
+        this.real = this.real + c.real;
+        this.imaginary = this.imaginary + c.imaginary;
+    }
+    
+    public void multiply(ComplexNumbers c){
+        int temp = this.real;
+        this.real = (this.real * c.real) - (this.imaginary * c.imaginary);
+        this.imaginary = (this.imaginary * c.real) + (c.imaginary * temp);
+    }
+    
+    public void print(){
+        System.out.println(this.real+" + i"+this.imaginary);
+    }
+    
+	
+}
